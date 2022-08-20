bc1b21e2afdd50fa03cc4d6435107df10e41d1ab
ztytotoro <ztytotoro@outlook.com>
2022-08-20 15:27:34 +0800

    css test

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 /** @jsx h */
 import { createElement as h } from "https://esm.sh/react@18.2.0";
+import "./App.css";
 
 export function App() {
-  return <div>Hello, World!</div>;
+  return <div className="app">Hello, World!</div>;
 }
