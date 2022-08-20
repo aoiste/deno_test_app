c187bb113e43b44a79dfdccd6c48d72afab7dc29
ztytotoro <ztytotoro@outlook.com>
2022-08-20 16:31:31 +0800

    hooks test

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 /** @jsx h */
-import { createElement as h } from "https://esm.sh/react@18.2.0";
+import { createElement as h, useState } from "https://esm.sh/react@18.2.0";
 import "./App.css";
 import image from "./logo.svg";
 
 export function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="app">
       Hello, World!
       <img src={image} alt="" />
+      {count}
+      <button onClick={() => setCount((c) => c - 1)}>-1</button>
+      <button onClick={() => setCount((c) => c + 1)}>+1</button>
     </div>
   );
 }
