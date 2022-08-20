785d1b7817297b3cdd65295dadd8925ff9a3f802
ztytotoro <ztytotoro@outlook.com>
2022-08-20 16:14:26 +0800

    asset test

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 /** @jsx h */
 import { createElement as h } from "https://esm.sh/react@18.2.0";
 import "./App.css";
+import image from "./logo.svg";
 
 export function App() {
-  return <div className="app">Hello, World!</div>;
+  return (
+    <div className="app">
+      Hello, World!
+      <img src={image} alt="" />
+    </div>
+  );
 }
