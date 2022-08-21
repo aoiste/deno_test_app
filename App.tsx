63186c53635c5468b95dfafff6efa002848b962d
ztytotoro <ztytotoro@outlook.com>
2022-08-21 13:23:00 +0800

    update imports

--- App.tsx
+++ App.tsx
@@ -2,17 +2,18 @@
 import { createElement as h, useState } from "https://esm.sh/react@18.2.0";
 import "./App.css";
 import image from "./logo.svg";
-import { useRedacState } from "https://deno.land/x/redac@0.1.0-alpha.3/react.ts";
+import { join } from "https://deno.land/std@0.152.0/path/posix.ts";
 
 export function App() {
-  const count = useRedacState(0);
+  const [count, setCount] = useState(0);
   return (
     <div className="app">
       Hello, World!
       <img src={image} alt="" />
-      {count.current}
-      <button onClick={() => count.current--}>-1</button>
-      <button onClick={() => count.current++}>+1</button>
+      {count}
+      {join("a", "b", "c")}
+      <button onClick={() => setCount((c) => c - 1)}>-1</button>
+      <button onClick={() => setCount((c) => c + 1)}>+1</button>
     </div>
   );
 }
