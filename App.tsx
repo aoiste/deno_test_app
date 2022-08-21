825040315ecb4f4d5455d0fdcf987856fb57f422
ztytotoro <ztytotoro@outlook.com>
2022-08-21 11:15:09 +0800

    deno.land/x test

--- App.tsx
+++ App.tsx
@@ -2,16 +2,17 @@
 import { createElement as h, useState } from "https://esm.sh/react@18.2.0";
 import "./App.css";
 import image from "./logo.svg";
+import { useRedacState } from "https://deno.land/x/redac@0.1.0-alpha.3/react.ts";
 
 export function App() {
-  const [count, setCount] = useState(0);
+  const count = useRedacState(0);
   return (
     <div className="app">
       Hello, World!
       <img src={image} alt="" />
-      {count}
-      <button onClick={() => setCount((c) => c - 1)}>-1</button>
-      <button onClick={() => setCount((c) => c + 1)}>+1</button>
+      {count.current}
+      <button onClick={() => count.current--}>-1</button>
+      <button onClick={() => count.current++}>+1</button>
     </div>
   );
 }
